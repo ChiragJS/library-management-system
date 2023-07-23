ac7e26805301b9a5c5ac110db1b91c01419db387
Chirag <chiragjaiswal2403@gmail.com>
2023-07-23 23:19:45 +0530

    test push

--- index.js
+++ index.js
@@ -8,13 +8,11 @@ const searchRouter = require ('./routes/search');
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-
 app.use('/members',membersRouter);
 app.use('/books',booksRouter);
 app.use('/search',searchRouter);
 app.use('/transaction',transactionRouter);
 mongoose.connect("mongodb://127.0.0.1:27017/LMS", { useNewUrlParser: true, useUnifiedTopology: true, dbName:"LMS" });
 app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port 3000");//push
 });
